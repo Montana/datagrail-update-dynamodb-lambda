26e55e84bcb839d9baee87f28b79a52959dca77a
Michael Mendy <montana@linux.com>
2024-05-22 07:40:25 -0700

    push handler function

---
+++ UpdateDynamoDBItem.js
@@ -0,0 +1,26 @@
+let AWS = require('aws-sdk');
+let ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    const params = {
+        TableName: 'your-table-name',
+        Key: { 'primaryKeyAttribute': event.primaryKeyValue },
+        UpdateExpression: 'set #attrName = :attrValue',
+        ExpressionAttributeNames: { '#attrName': 'attributeName' },
+        ExpressionAttributeValues: { ':attrValue': event.newValue },
+        ReturnValues: 'UPDATED_NEW'
+    };
+
+    try {
+        const data = await ddb.update(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Attributes)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+};
